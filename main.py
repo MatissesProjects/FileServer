ed98292ed9f55195c2a3acfad516cfe21b454eb6
Matisse <MatissesProjects@users.noreply.github.com>
2023-12-24 12:47:01 -0800

    removed bad endpoint added upload

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-from flask import Flask
-import flask
-from os import listdir
+from flask import Flask, request, Response, send_from_directory
 app = Flask(__name__)
 
 basePath="/home/matisse/outputfiles/"
 
-@app.get("/")
-async def getFiles():
-    return listdir(basePath+ "images/")
-
 @app.get("/output/createImage/<int:fileNumber>")
 async def outputImage(fileNumber):
     filename = f'{fileNumber}.png'
-    return flask.send_from_directory(basePath + "images/", filename)
+    return send_from_directory(basePath + "images/", filename)
+
+@app.post("/uploadImage")
+async def uploadImage():
+    files = request.files
+    print(files)
+    if 'imageFile' not in files:
+        return Response(response='failed to get image', status=501)
+    
+    file = files['imageFile']
+    if file.filename == '':
+        print("no image file")
+        return Response(response=f'need to supply a file name', status=502)
+
+    print("success")
+    filePath = f'{basePath}images/{file.filename}.png'
+    print(f"saving to {filePath}")
+    await file.save(filePath)
+    return Response(response=f'success', status=200)
 
 app.run("127.0.0.1", "5113")
